790ad1b0a309d9e8a1e98a24ef35e0300092b796
Aaron <ayybuddyy@gmail.com>
2021-06-09 00:19:48 -0400

    Add image and image overlay animations

--- src/components/animations/animations.js
+++ src/components/animations/animations.js
@@ -20,11 +20,15 @@ export const pageTransitionFadeOut = {
 // This is the parent container
 // Delayed extra long to show animation
 export const fadeUpParent = {
-  hidden: { opacity: 0 },
-  show: {
+  hidden: { opacity: 0, y: "100%" },
+  visible: {
     opacity: 1,
+    y: 0,
     transition: {
-      delay: 2,
+      type: "tween",
+      ease: "easeInOut",
+      duration: 2,
+      delay: 1.2,
       when: "beforeChildren",
       staggerChildren: 0.5,
     },
@@ -35,18 +39,139 @@ export const fadeUpParent = {
 // This is the child elements
 export const fadeUpChild = {
   hidden: { opacity: 0, y: "100%" },
-  show: { opacity: 1, y: 0 },
+  visible: { opacity: 1, y: 0 },
 };
 
 // Fades Up from bottom
 export const fadeUp = {
-  hidden: { y: "100px", opacity: 0 },
+  hidden: { y: "100%", opacity: 0 },
   visible: {
     y: 0,
     opacity: 1,
     transition: {
       duration: 1,
       delay: 1,
+    },
+  },
+};
+
+// Fades Down from top
+export const fadeDown = {
+  hidden: { y: "-100%", opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      delay: 1,
+    },
+  },
+};
+
+// Fades In from the Left
+export const fadeFromLeft = {
+  hidden: { opacity: 0, x: "-100%" },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 1,
+      delay: 1,
+    },
+  },
+};
+
+// Fades In from the Right
+export const fadeFromRight = {
+  hidden: { opacity: 0, x: "100%" },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 1,
+      delay: 1,
+    },
+  },
+};
+
+export const changeSize = {
+  // hidden: { width: "100%", x: 0 },
+  // visible: {
+  //   width: 0,
+  //   x: "100%",
+  //   transition: {
+  //     duration: 1,
+  //     delay: 1,
+  //   },
+  // },
+};
+
+// Slides in from Left to Right 50% of width
+// The container needs to be positioned absolute
+export const leftToRight50 = {
+  hidden: { width: "0%" },
+  visible: {
+    width: "50%",
+    transition: {
+      delay: 0.8,
+      duration: 2,
+      ease: "easeInOut",
+    },
+  },
+};
+
+export const leftToRightDesiredWidth = {
+  hidden: { width: 0 },
+  visible: {
+    width: "500px",
+    transition: {
+      delay: 1.2,
+      duration: 0.5,
+      ease: "easeInOut",
+    },
+  },
+};
+
+// Slides in from Right to Left 50% of width
+// The container needs to be positioned absolute
+export const rightToLeft50 = {
+  hidden: { width: "0%" },
+  visible: {
+    width: "50%",
+    transition: {
+      delay: 0.8,
+      duration: 2,
+      ease: "easeInOut",
+    },
+  },
+};
+
+// The container needs to be positioned absolute
+// Height set to 100% of parent, background-color set
+// Top position set to 0
+export const imageOverlayLeftToRight = {
+  hidden: { left: 0, width: "0%" },
+  visible: {
+    left: "100%",
+    width: ["0%", "100%", "0%"],
+    transition: {
+      duration: 1.8,
+      ease: "easeInOut",
+      delay: 0.5,
+    },
+  },
+};
+
+// Fix, does not work, has to do with block position
+export const imageOverlayTopToBottom = {
+  hidden: { top: 0, height: "0%" },
+  visible: {
+    top: "100%",
+    height: ["0%", "100%", "0%"],
+    transition: {
+      duration: 1.8,
+      ease: "easeInOut",
+      delay: 0.5,
     },
   },
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,6 +6,16 @@ import {
   fadeUpParent,
   fadeUpChild,
   pageTransitionFadeOut,
+  fadeUp,
+  fadeDown,
+  fadeFromLeft,
+  fadeFromRight,
+  changeSize,
+  leftToRight50,
+  rightToLeft50,
+  leftToRightDesiredWidth,
+  imageOverlayLeftToRight,
+  imageOverlayTopToBottom
 } from "../../components/animations/animations";
 import './Home.css'
 const HomeContainer = styled(motion.section)`
@@ -28,12 +38,22 @@ const Home = () => {
         animate="visible"
         exit="exit"
       >
-        <motion.div className='parentContainer' variants={fadeUpParent} initial="hidden" animate="show">
+        <motion.div className='parentContainer' variants={fadeUpParent} initial="hidden" animate="visible">
           <MainHeading variants={fadeUpChild}>The</MainHeading>
           <MainHeading variants={fadeUpChild}>Home</MainHeading>
           <MainHeading variants={fadeUpChild}>Page</MainHeading>
-          
         </motion.div>
+        <div className="wrapper">
+          <motion.div 
+          className="left" variants={leftToRight50} initial='hidden' animate='visible'></motion.div>
+          <motion.div className="right" variants={rightToLeft50} initial='hidden' animate='visible'></motion.div>
+
+          <div className="img-wrapper-pic">
+            <motion.img src="https://images.unsplash.com/photo-1518709766631-a6a7f45921c3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1525&q=80" alt="" className="gazelle" variants={leftToRightDesiredWidth} initial='hidden' animate='visible' />
+            <motion.div className="block" 
+            variants={imageOverlayLeftToRight} initial='hidden' animate='visible'></motion.div>
+          </div>
+        </div>
       </HomeContainer>
     </Layout>
   );
